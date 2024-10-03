2c0b28c82051d1dca79af1f711d1f56ff61c0c39
junaid akhter <junaidakhter945@gmail.com>
2024-10-03 10:00:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// chapter 17-20
+
+// q1
+
+// let multiArray = [[]]; 
+// multiArray[0].push(1, 2, 3); 
+// multiArray.push([4, 5, 6]);  
+
+// console.log(multiArray);
+
+
+
+// q3
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// q5
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// chapter 21-25
+// q1
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName + " " + lastName;
+// alert("Hello, " + fullName + "! Welcome.");
+
+// q2
+
+// let favoritePhone = prompt("What is your favorite mobile phone model?");
+// let inputLength = favoritePhone.length;
+
+// document.write("Your favorite mobile phone model is: " + favoritePhone + "<br>");
+// document.write("The length of your input is: " + inputLength);
+
+
+// chapter 25-30
+
+// q1
+// let userInput = parseFloat(prompt("Enter a positive number:"));
+
+// document.write("Number: " + userInput + "<br>");
+// document.write("Round off value: " + Math.round(userInput) + "<br>");
+// document.write("Floor value: " + Math.floor(userInput) + "<br>");
+// document.write("Ceil value: " + Math.ceil(userInput));
+
+// q3
+
+// let userInput = parseFloat(prompt("Enter a number:"));
+// document.write("The absolute value of " + userInput + " is: " + Math.abs(userInput));
+
